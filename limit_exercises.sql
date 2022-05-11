5eb9a913ab154470c9fccab2997f9cf1c6904c55
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-05-11 12:16:18 -0500

    updates

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,29 +1,47 @@
+/*
+__/\\\\\\__________________________________________________        
+ _\////\\\__________________________________________________       
+  ____\/\\\_____/\\\______________________/\\\_____/\\\______      
+   ____\/\\\____\///_____/\\\\\__/\\\\\___\///___/\\\\\\\\\\\_     
+    ____\/\\\_____/\\\__/\\\///\\\\\///\\\__/\\\_\////\\\////__    
+     ____\/\\\____\/\\\_\/\\\_\//\\\__\/\\\_\/\\\____\/\\\______   
+      ____\/\\\____\/\\\_\/\\\__\/\\\__\/\\\_\/\\\____\/\\\_/\\__  
+       __/\\\\\\\\\_\/\\\_\/\\\__\/\\\__\/\\\_\/\\\____\//\\\\\___ 
+        _\/////////__\///__\///___\///___\///__\///______\/////____
+
+*/
 -- 1. Create a new SQL script named limit_exercises.sql.
 USE employees;
 
 -- 2. MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. For example, to find all the unique titles within the company, we could run the following query: SELECT DISTINCT title FROM titles;
 
 --  List the first 10 distinct last name sorted in descending order.
-SELECT DISTINCT last_name FROM employees
+SELECT DISTINCT last_name 
+    FROM employees
 ORDER BY last_name DESC
-LIMIT 10; #Zykh, Zyda, Zwicker, Zweizig, Zumaque, Zultner, Zucker, Zuberek, Zschoche, Zongker
-
+LIMIT 10; -- prints Zykh, Zyda, Zwicker, Zweizig, Zumaque, Zultner, Zucker, Zuberek, Zschoche, Zongker
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
 
-SELECT * FROM employees 
-WHERE hire_date LIKE '199%'
-	AND birth_date LIKE '%-12-25'
+SELECT * 
+    FROM employees 
+    WHERE hire_date LIKE '199%'
+        AND birth_date LIKE '%-12-25'
 ORDER BY hire_date
 LIMIT 5; #Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup 
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 4. Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results.
 
-SELECT * FROM employees 
-WHERE hire_date LIKE '199%'
-	AND birth_date LIKE '%-12-25'
+SELECT * 
+    FROM employees 
+    WHERE hire_date LIKE '199%'
+        AND birth_date LIKE '%-12-25'
 ORDER BY hire_date
 LIMIT 5 OFFSET 45; # Bingo was his name-o. 
 
 -- LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
 
-# OFFSET can be seen as flipping ahead to a particular 'page,' batch or grouping, and the amount of content appearing on this 'page' is determined by the LIMIT. 
+-- OFFSET can be seen as flipping ahead to a particular 'page,' batch or grouping, 
+-- and the amount of content appearing on this 'page' is determined by the LIMIT. 
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,22 @@
+/*
+=======================================================
+==================  ===================  ==============
+==================  ===================  ==============
+==================  ===================  ==============
+==   ==  =   =====  ==   ==  =   ======  ====  =  =====
+=     =    =  ==    =  =  =    =  =====    ==  =  =====
+=  =  =  ======  =  =     =  ==========  =  ==    =====
+=  =  =  ======  =  =  ====  ==========  =  ====  =====
+=  =  =  ======  =  =  =  =  ==========  =  =  =  =====
+==   ==  =======    ==   ==  ==========    ===   ======
+=======================================================
+*/
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 1. Create a new file named order_by_exercises.sql and copy in the
 --   contents of your exercise FROM the previous lesson.
                         # WILCO
 -- -------------------------------------------------------------------------------------------------------
-                        
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------                        
 -- 2. Find all employees with first names 'Irena', 'Vidya', or 'Maya', 
 --   AND order your results returned BY FIRST name. IN your comments, 
 --   answer: What was the FIRST AND LAST NAME IN the FIRST ROW of the results? 
@@ -19,7 +33,7 @@ ORDER BY first_name; # 709.
 DERP. 					Irena Reutenauer. Vidya Simmen.
 */
 -- -------------------------------------------------------------------------------------------------------
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 3. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results
 --    returned BY FIRST NAME AND THEN LAST name. IN your comments, answer: What was the FIRST 
 --   AND LAST NAME IN the FIRST ROW of the results? What was the FIRST AND LAST NAME of the LAST person IN the TABLE?
@@ -30,7 +44,7 @@ SELECT *
    IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name, last_name; #Irena Acton for first row. Vidya Zweizig as the last row. 
 -- -------------------------------------------------------------------------------------------------------
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 4. Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results 
 --   returned BY LAST NAME AND THEN FIRST name. IN your comments, answer: What was the FIRST 
 --   AND LAST NAME IN the FIRST ROW of the results? What was the FIRST AND LAST NAME of the 
@@ -42,7 +56,7 @@ SELECT *
     IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name, first_name; # Row 1 = Irena Acton ; Final Row (709) = Maya Zyda
 -- -------------------------------------------------------------------------------------------------------
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 5. Write a query to to find all employees whose last name starts and ends with 'E'. 
 --   Sort the results BY their employee number. Enter a COMMENT WITH the number of employees returned,
 --    the FIRST employee number AND their FIRST AND LAST NAME, AND the LAST employee number WITH their 
@@ -55,7 +69,7 @@ SELECT *
 ORDER BY emp_no DESC; # 899 results. 
 -- ROW 1: 499648 Tadahiro Erde ; ROW 899: 10021 Ramzi Erde
 -- -------------------------------------------------------------------------------------------------------
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 6. Write a query to  find all employees whose last name starts and ends with 'E'. Sort the results
 --    BY their hire DATE, so that the newest employees are listed first. Enter a COMMENT WITH the number 
 --    of employees returned, the NAME of the newest employee, AND the NAME of the oldest employee.
@@ -67,7 +81,7 @@ SELECT *
 ORDER BY hire_date DESC; # 899 results.
 --   Newest: Teiji Eldridge ; Oldest: Sergi Erde
 -- -------------------------------------------------------------------------------------------------------
-
+--  ---  ----  -----  ------  --------  -----------  ------------  ----------------  ------------------
 -- 7. Find all employees hired in the 90s and born on Christmas. Sort the results so that the oldest employee
 --   who was hired LAST IS the FIRST result. Enter a COMMENT WITH the number of employees returned, the NAME 
 --   of the oldest employee who was hired LAST, AND the NAME of the youngest employee who was hired first.
@@ -77,4 +91,4 @@ SELECT *
 	WHERE birth_date LIKE '%-12-25'
 		  AND hire_date LIKE '199%'
 ORDER BY birth_date ASC, hire_date DESC; #362 
-# Oldest person, most recent hire: Khun Bernini Youngest person, older hire: Douadi Pettis
+-- Oldest person, most recent hire: Khun Bernini Youngest person, older hire: Douadi Pettis
